918a2a3e5748d67b9681abffb078a6ce218ccceb
Juan Manuel Sardoy <jmsardoy@gmail.com>
2017-10-22 19:18:10 -0300

    build frontend app

--- Web/backend/app.py
+++ Web/backend/app.py
@@ -1,16 +1,16 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template
 from db import DBManager
 from api import api_bp, websocket
 from flask_cors import CORS
 from flask import Blueprint
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='public')
 app.register_blueprint(api_bp, url_prefix='/api')
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/')
 def index():
-    return "INDEX"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     websocket.init_app(app)
