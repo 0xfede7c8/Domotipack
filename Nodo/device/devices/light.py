ef949d2c574e84349639b66b52e0ee3ef6b57e54
Federico Perez <perezfederico@unc.edu.ar>
2017-11-20 15:56:46 -0300

    asfd

--- Nodo/device/devices/light.py
+++ Nodo/device/devices/light.py
@@ -1,5 +1,10 @@
 from ..device import Device
 from time import sleep
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT) ## GPIO 17 como salida
+GPIO.setup(27, GPIO.OUT) ## GPIO 27 como salida
 
 class Light(Device):
 
@@ -8,7 +13,19 @@ class Light(Device):
 
     #when a change is made from the web
     def apply_state(self):
-        print "overwritten apply_state", self.state
+        print self.state
+         print "Ejecucion iniciada..."
+        iteracion = 0
+        while iteracion < 1: ## Segundos que durara la funcion
+                GPIO.output(17, True) ## Enciendo el 17
+                GPIO.output(27, False) ## Apago el 27
+                time.sleep(1) ## Esperamos 1 segundo
+                GPIO.output(17, False) ## Apago el 17
+                GPIO.output(27, True) ## Enciendo el 27
+                time.sleep(1) ## Esperamos 1 segundo
+                iteracion = iteracion + 2 ## Sumo 2 porque he hecho dos parpadeos
+        print "Ejecucion finalizada"
+        GPIO.cleanup() ## Hago una limpieza de los GPIO
 
     #monitors changes from the devices
     def monitor_changes(self):
