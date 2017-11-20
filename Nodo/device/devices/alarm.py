d593ae99646481f25b0a8d705939c7a0108693bc
Federico Perez <perezfederico@unc.edu.ar>
2017-11-20 17:47:02 -0300

    asfd

--- Nodo/device/devices/alarm.py
+++ Nodo/device/devices/alarm.py
@@ -7,7 +7,7 @@ GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 class Alarm(Device):
     def __init__(self, *args):
         Device.__init__(self, *args)
-        GPIO.add_event_detect(23, GPIO.RISING, callback=alarmActivated, bouncetime=300)
+        GPIO.add_event_detect(23, GPIO.RISING, callback=self.alarmActivated, bouncetime=300)
 
     def apply_state(self):
         print self.state
