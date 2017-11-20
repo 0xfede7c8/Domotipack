96300ca5c5fd30438fa014acb4fb444b5710486d
Juan Manuel Sardoy <jmsardoy@gmail.com>
2017-11-20 19:02:47 -0300

    added alarm on frontend

--- Web/frontend/src/components/devices/device_alarm.js
+++ Web/frontend/src/components/devices/device_alarm.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react'
 import Toggle from 'material-ui/Toggle'
 
 export default class Alarm extends Component {
+    handleToggle(event, is_checked){
+        const device_state = Object.assign(this.props.device.state, {armed:is_checked});
+        const device = Object.assign(this.props.device,{state: device_state});
+        this.props.onStateChange(device);
+    }
+    handleSlider(event, new_value){
+        const device_state= Object.assign(this.props.device.state, {value:new_value});
+        const device = Object.assign(this.props.device,{state: device_state});
+        this.props.onStateChange(device);
+    }
+
     render(){
         return (
             <div>
@@ -10,8 +21,13 @@ export default class Alarm extends Component {
                         ENABLE
                     </div>
                     <div className="col-md-6">
-                        <Toggle />
+                        <Toggle 
+                            onToggle={this.handleToggle.bind(this)}
+                        />
                     </div>
+                </div>
+                <div className="row">
+                    {this.props.device.state.active}
                 </div>
             </div>
         );
