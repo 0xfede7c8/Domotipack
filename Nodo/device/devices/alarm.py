0b17d9db38b1e9ad3be5963e7c1b2d15ed88ef3d
Federico Perez <perezfederico@unc.edu.ar>
2017-11-20 17:43:16 -0300

    asfd

--- Nodo/device/devices/alarm.py
+++ Nodo/device/devices/alarm.py
@@ -1,12 +1,19 @@
 from ..device import Device
 from time import sleep
 
+import RPi.GPIO as GPIO
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 class Alarm(Device):
     def __init__(self, *args):
         Device.__init__(self, *args)
+        GPIO.add_event_detect(23, GPIO.RISING, callback=alarmActivated, bouncetime=300)
 
     def apply_state(self):
-        print "overwritten apply_state", self.state
+        print self.state
 
     def monitor_changes(self):
         sleep(1)
+
+	def alarmActivated(self, channel):  
+		print "AlarmActivated" 
