6671fdbfdc0d447862afda8ec7b4503877106210
Federico Perez <perezfederico@unc.edu.ar>
2017-11-20 16:34:17 -0300

    asfd

--- Nodo/device/devices/light.py
+++ Nodo/device/devices/light.py
@@ -10,24 +10,28 @@ class Light(Device):
 
     def __init__(self, *args):
         Device.__init__(self, *args)
+        self.on = 
 
     #when a change is made from the web
     def apply_state(self):
-        
-        if self.state["state"]["on"]:
-            print "Encendido."          
-            GPIO.output(17, True) ## Enciendo el 17
-        elif not self.state["state"]["on"]:
-            print "Apagado."
-            GPIO.output(17, False) ## Apago el 17
-
+        pass
         #GPIO.cleanup() ## Hago una limpieza de los GPIO
         
     #monitors changes from the devices
     def monitor_changes(self):
-        for i in range(10):
-            if not self.kill_received:
-                sleep(2)
-                self.state['state']['value'] += 10
-                self.devices_state.set_device(self.state, notify_server=True)
-        self.kill_received = True
+        #self.state['state']['value'] += 10
+        if self.state["state"]["on"]:
+            self.strobe(self.state["state"]["value"])
+        else
+            GPIO.output(17, False) ## Apago el 17
+        #self.devices_state.set_device(self.state, notify_server=True) 
+
+    def strobe(width):
+        factor = 1000
+        GPIO.output(17, True) ## Enciendo el 17
+        timeoff =  100 - width
+        sleep(width / factor)
+        GPIO.output(17, False) ## Apago el 17
+        sleep(timeoff / factor)
+
+
