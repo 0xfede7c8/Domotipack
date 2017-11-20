efb033e71525c11bec73a450e23215f8baa07a0c
Federico Perez <perezfederico@unc.edu.ar>
2017-11-20 16:38:42 -0300

    asfd

--- Nodo/device/devices/light.py
+++ Nodo/device/devices/light.py
@@ -25,7 +25,7 @@ class Light(Device):
         #self.devices_state.set_device(self.state, notify_server=True) 
 
     def strobe(self, width):
-        factor = 1000
+        factor = 10
         GPIO.output(17, True) ## Enciendo el 17
         timeoff =  100 - width
         sleep(width / factor)
