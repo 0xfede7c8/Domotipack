30ef73527bc0a247b02abe2c7cab51da9c31a21d
Federico Perez <perezfederico@unc.edu.ar>
2017-11-20 17:55:11 -0300

    asfd

--- Nodo/device/devices/alarm.py
+++ Nodo/device/devices/alarm.py
@@ -2,13 +2,12 @@ from ..device import Device
 from time import sleep
 
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 class Alarm(Device):
     def __init__(self, *args):
         Device.__init__(self, *args)
         try:
+        	GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         	GPIO.add_event_detect(23, GPIO.RISING, callback=self.alarmActivated, bouncetime=300)
         except:
         	print "caca"
