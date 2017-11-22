fffcf0f3138f3548e35251f8798c4631dd9090b9
Juan Manuel Sardoy <jmsardoy@gmail.com>
2017-11-22 20:23:06 -0300

    asd

--- Nodo/device/jsons.py
+++ Nodo/device/jsons.py
@@ -1,9 +1,9 @@
 def light_json(ip, default_value = 0):
     return {"ip": ip, 
-            "state": {"on": True, "value": default_value},
+            "state": {"on": False, "value": default_value},
             "type": "light"}
 
 def alarm_json(ip):
     return {"ip": ip,
-            "state": {"active": False, "armed": True}, 
+            "state": {"active": False, "armed": False}, 
             "type":"alarm"}
