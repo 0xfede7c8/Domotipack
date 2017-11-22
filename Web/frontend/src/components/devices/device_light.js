d1856aa367197ad3dcc12558f70e077910278698
Juan Manuel Sardoy <jmsardoy@gmail.com>
2017-11-22 20:42:38 -0300

    asd

--- Web/frontend/src/components/devices/device_light.js
+++ Web/frontend/src/components/devices/device_light.js
@@ -16,6 +16,7 @@ export default class Light extends Component{
         var new_value;
         if (is_checked){new_value = 100;}
         else{new_value = 0;}
+        this.props.device.state.value = new_value;
         const device_state= Object.assign(this.props.device.state,
             {value: new_value, on:is_checked});
         const device = Object.assign(this.props.device,{state: device_state});
